ad7e67413433471c4c6513157db4f29e09d72da1
heifade <heifade@126.com>
2017-12-18 22:20:16 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { app } from "server-i";
+
+
+let server = app.listen(3000, function() {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log("Server listening at http://%s:%s", host, port);
+});
